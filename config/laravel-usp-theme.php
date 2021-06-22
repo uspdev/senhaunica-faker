aca206e706b372e38ac1a2d10abbe0ac4e71ac9d
Masaki Kawabata Neto <kawabata@usp.br>
2021-06-22 18:53:51 -0300

    atualizando usptheme para v2

--- config/laravel-usp-theme.php
+++ config/laravel-usp-theme.php
@@ -1,11 +1,26 @@
 <?php
 
 return [
+    # valor default para a tag title, dentro da section title.
+    # valor pode ser substituido pela aplicação.
     'title' => config('app.name'),
-    'dashboard_url' => config('app.url'),
+
+    # USP_THEME_SKIN deve ser colocado no .env da aplicação 
+    'skin' => env('USP_THEME_SKIN', 'uspdev'),
+
+    # chave da sessão. Troque em caso de colisão com outra variável de sessão.
+    'session_key' => 'laravel-usp-theme',
+
+    # usado na tag base, permite usar caminhos relativos nos menus e demais elementos html
+    # na versão 1 era dashboard_url
+    'app_url' => config('app.url'),
+
+    # login e logout
     'logout_method' => 'POST',
-    'logout_url' => config('app.url') . '/',
-    'login_url' => config('app.url') . '/',
+    'logout_url' => 'logout',
+    'login_url' => 'login',
+
+    # menus
     'menu' => [],
-    'right_menu' => []
+    'right_menu' => [],
 ];
