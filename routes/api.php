fdabd9e9acd95205a42adbc7175acd934f2a4245
Masaki Kawabata Neto <kawabata@usp.br>
2025-08-18 16:28:53 -0300

    cleanup

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
 
 use App\Http\Controllers\AccessTokenController;
-use App\Http\Controllers\AuthorizationController;
 use App\Http\Controllers\RequestTokenController;
 use App\Http\Controllers\UsuarioUSPController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
